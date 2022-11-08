9ddaa18dff45438a2f07a86d0e3a915117e6d1c7
vpremani-mss <vrutika.premani12@gmail.com>
2022-11-08 09:20:07 -0500

    components and packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DataTableComponent } from './components/data-table/data-table.component';
+import { DoughnutChartComponent } from './components/doughnut-chart/doughnut-chart.component';
+import { BarChartComponent } from './components/bar-chart/bar-chart.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DataTableComponent,
+    DoughnutChartComponent,
+    BarChartComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
